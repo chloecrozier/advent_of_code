3f30b29bd8795009a86c2118a084e8da9d67f1d3
chloecrozier <chloecrozier@gmail.com>
2025-12-09 20:58:08 -0800

    adding id screenshot

---
+++ 2025/day3.py
@@ -0,0 +1,10 @@
+# Run with: python day{#}.py {INPUT_PATH}, eg. python 2025/day3.py input/2025/day3.txt
+import sys
+data = (open(sys.argv[1], 'r')).readlines()
+print(data)
+
+ans1 = 0
+ans2 = 0
+# print('Part 1 Answer: ', ans1)
+# print('Part 2 Answer: ', ans2)
+# print('')
