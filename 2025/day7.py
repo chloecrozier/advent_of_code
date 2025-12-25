589025bad329cca1e139737e97ce1fbf2422c9ad
chloecrozier <chloecrozier@gmail.com>
2025-12-24 23:58:58 -0600

    part of 7.1

--- 2025/day7.py
+++ 2025/day7.py
@@ -1,12 +1,20 @@
-# Run with: python day{#}.py {INPUT_PATH}, eg. python 2025/day6.py input/2025/day6.txt
+# Run with: python day{#}.py {INPUT_PATH}, eg. python 2025/day7.py input/2025/day7.txt
 import sys
 import re
 data = (open(sys.argv[1], 'r')).readlines()
+splits = [0] * len(data[0])
+splits[data[0].index('S')] = 1
 
-nums1 = []
-for i in range(len(data) - 1):
-    nums1.append(re.findall(r'\S+', data[i]))
-operations = re.findall(r'\S+', data[-1])
+print(splits)
+
+for i in range(1, len(data)):
+    print(i)
+# nums1 = []
+# for i in range(len(data) - 1):
+#     nums1.append(re.findall(r'\S+', data[i]))
+# operations = re.findall(r'\S+', data[-1])
+
+
 
 ans1 = 0
 ans2 = 0
